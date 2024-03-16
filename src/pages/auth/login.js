dfd51f71b80da4b6de4074e6bf85ef298052e9c7
RahulKathayat <rkathayat1103@gmail.com>
2024-03-16 17:13:49 +0530

    auth bug fixed

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -16,12 +16,12 @@ import {
   TextField,
   Typography
 } from '@mui/material';
-// import { useAuth } from 'src/hooks/use-auth';
+import { useAuth } from 'src/hooks/use-auth';
 import { Layout as AuthLayout } from 'src/layouts/auth/layout';
 import axios from 'axios';
 const Page = () => {
   const router = useRouter();
-  // const auth = useAuth();
+  const auth = useAuth();
   const [method, setMethod] = useState('email');
   const formik = useFormik({
     initialValues: {
@@ -42,7 +42,7 @@ const Page = () => {
     }),
     onSubmit: async (values, helpers) => {
       try {
-        // await auth.signIn(values.email, values.password);
+          await auth.signIn(values.email, values.password);
           console.log(values);
           const user = {email: values.email, password: values.password}
           await axios
@@ -82,7 +82,7 @@ const Page = () => {
       auth.skip();
       router.push('/');
     },
-    // [auth, router]
+    [auth, router]
   );
 
   return (
